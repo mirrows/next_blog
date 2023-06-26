8812fddc61c5f4f999e502f4ed64fc0caae7f3e7
huangweiye <huangweiye@chengyun.biz>
2023-06-26 14:38:43 +0800

    修改加载规则

--- components/LazyImage/index.tsx
+++ components/LazyImage/index.tsx
@@ -10,41 +10,51 @@ type TProps = {
 function LazyImage({ src, className, ...props }: TProps) {
     const loadingGif = useRef(process.env.NEXT_PUBLIC_LOADING_GIF || 'https://empty.t-n.top/pub_lic/2023_06_09/pic1686281264582557.gif')
     const failImg = useRef('https://empty.t-n.top/pub_lic/2023_06_26/pic1687748007844003.png')
-    const [imgSrc, setSrc] = useState(src)
+    const [imgSrc, setSrc] = useState(loadingGif.current)
     const imgRef = useRef<HTMLImageElement | null>(null) 
     const handleError = () => {
         setSrc(failImg.current)
     }
-    // useEffect(() => {
-    //     const clientHeight = document.documentElement.clientHeight
-    //     const clientWidth = document.documentElement.clientWidth
-    //     imgRef.current?.classList.add('lazy')
-    //     if(
-    //         imgRef.current 
-    //         && !(imgRef.current.getBoundingClientRect().top < -imgRef.current.clientHeight
-    //         || imgRef.current.getBoundingClientRect().top > 1.5 * clientHeight)
-    //         && !(imgRef.current.getBoundingClientRect().left < -clientWidth
-    //         || imgRef.current.getBoundingClientRect().left > 1.5 * clientWidth)
-    //     ) {
-    //         setSrc(src)
-    //         imgRef.current?.classList.remove('lazy')
-    //     }
-    // }, [src])
+    useEffect(() => {
+        const clientHeight = document.documentElement.clientHeight
+        const clientWidth = document.documentElement.clientWidth
+        imgRef.current?.classList.add('lazy')
+        if(
+            imgRef.current 
+            && !(imgRef.current.getBoundingClientRect().top < -imgRef.current.clientHeight
+            || imgRef.current.getBoundingClientRect().top > 1.5 * clientHeight)
+            && !(imgRef.current.getBoundingClientRect().left < -clientWidth
+            || imgRef.current.getBoundingClientRect().left > 1.5 * clientWidth)
+        ) {
+            setSrc(src)
+            imgRef.current?.classList.remove('lazy')
+        }
+    }, [src])
     return(
         <Image
-            // className={className ? `lazy ${className}` : 'lazy'}
-            className={className}
+            className={className ? `lazy ${className}` : 'lazy'}
             ref={imgRef}
             src={imgSrc}
-            // src={imgSrc}
-            // data-src={src}
+            data-src={src}
             alt=""
-            placeholder="blur"
-            blurDataURL={loadingGif.current}
-            loading="lazy"
             onError={handleError}
             {...props}
         />
+        // <Image
+        //     // className={className ? `lazy ${className}` : 'lazy'}
+        //     className={className}
+        //     ref={imgRef}
+        //     src={imgSrc}
+        //     // src={imgSrc}
+        //     // data-src={src}
+        //     alt=""
+        //     lazyBoundary=""
+        //     placeholder="blur"
+        //     blurDataURL={loadingGif.current}
+        //     loading="lazy"
+        //     onError={handleError}
+        //     {...props}
+        // />
     )
 }
 
