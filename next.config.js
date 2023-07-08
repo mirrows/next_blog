cca11674462eaef29e0588ba8cb6c737136e26b1
huangweiye <huangweiye@chengyun.biz>
2023-07-08 12:17:12 +0800

    sdss

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  "plugins": ["babel-plugin-styled-components"],
   images: {
     remotePatterns: [
       {
