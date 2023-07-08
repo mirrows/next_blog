484f1b1de9a394c785e3a6548ec19b550b1a5baf
huangweiye <huangweiye@chengyun.biz>
2023-07-08 12:03:52 +0800

    6666

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  "plugins": ["babel-plugin-styled-components"],
   images: {
     remotePatterns: [
       {
